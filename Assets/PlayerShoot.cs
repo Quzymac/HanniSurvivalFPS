3c8ee08195e8c1584e68813dea13d73c40973d38
Quzymac <33623813+Quzymac@users.noreply.github.com>
2018-08-15 21:07:14 +0200

    playershoot

---
+++ Assets/PlayerShoot.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour {
+
+    public PlayerWeapon weapon;
+
+    [SerializeField] Camera cam;
+
+    [SerializeField] LayerMask mask;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, weapon.range, mask))
+        {
+            //we hit something
+            if(hit.collider.tag == "Enemy")
+            {
+                EnemyShot(hit.collider.name);
+            }
+        }
+    }
+    //Move this to enemy manager
+    void EnemyShot(string name)
+    {
+        Debug.Log(name + " has been shot!");
+    }
+}
