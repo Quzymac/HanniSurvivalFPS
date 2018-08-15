4e2bd60d69d6698a33638c527ff9baafdf02e7ea
HanniBun <johannanas.nilsson@gmail.com>
2018-08-15 17:37:56 +0200

    Enemies now follow player!

---
+++ Assets/Scripts/EnemyTargetSeeking.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyTargetSeeking : MonoBehaviour {
+
+    public GameObject myTarget;
+    NavMeshAgent thisAgent;
+
+    private void Start()
+    {
+        thisAgent = gameObject.GetComponent<NavMeshAgent>();
+        myTarget = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update()
+    {
+        thisAgent.destination = myTarget.transform.position; 
+    }
+
+}
